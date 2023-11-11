1f1a3bdfb7eb158ec3112bdea6c9ba0cf90ae409
Troy Dutton <45671229+Bushvacka@users.noreply.github.com>
2023-11-11 15:37:13 -0600

    Non-working checkout

--- backend/server.py
+++ backend/server.py
@@ -3,100 +3,43 @@ import database
 
 app = Flask(__name__)
 
-
-@app.route("/login", methods=["POST"])
+@app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-
-    user = data.get("user")
-    password = data.get("password")
+    
+    user = data.get('user')
+    password = data.get('password')
 
     success = database.login_user(user, password)
 
     return jsonify({"success": success})
-
-
-@app.route("/logout", methods=["POST"])
+    
+@app.route('/logout', methods=['POST'])
 def logout():
     data = request.get_json()
-
-    user = data.get("user")
+    
+    user = data.get('user')
 
     success = database.logout_user(user)
 
     return jsonify({"success": success})
 
 
-@app.route("/signup", methods=["POST"])
+@app.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
-
-    user = data.get("user")
-    password = data.get("password")
+    
+    user = data.get('user')
+    password = data.get('password')
 
     success = database.create_user(user, password)
-
-    return jsonify({"success": success})
-
-@app.route('/create-project', methods=['POST'])
-def create_project():
-    data = request.get_json()
-
-    project_name = data.get('projectName')
-    user = data.get('user')
-
-    success = database.create_project(project_name, user)
     
     return jsonify({"success": success})
 
-@app.route('/join-project', methods=['POST'])
-def join_project():
-    data = request.get_json()
-
-    project_id = data.get('projectID')
-    user = data.get('user')
-
-    if project_id == None or user == None:
-        return jsonify({"success": False})
-
-    success = database.add_user_to_project(int(project_id), user)
-
-    return jsonify({"success": success})
-
-
-# functions for project manager page
-app.route("/checkIn_hardware/<int:projectId>/<int:qty>", methods=["GET"])
-def checkIn_hardware(projectId, qty):
-    # Perform operations like decrementing available hardware quantity
-    # and interacting with MongoDB if needed.
-    return jsonify({"message": f"{qty} hardware checked in"})
-
-
-@app.route("/checkOut_hardware/<int:projectId>/<int:qty>", methods=["GET"])
-def checkOut_hardware(projectId, qty):
-    # Perform operations like decrementing available hardware quantity
-    # and interacting with MongoDB if needed.
-    return jsonify({"message": f"{qty} hardware checked out"})
-
-
-@app.route("/joinProject/<int:projectId>", methods=["GET"])
-def joinProject(projectId):
-    # Perform operations like checking if the user is authorized to join
-    # and interacting with MongoDB if needed.
-    return jsonify({"message": f"Joined Project {projectId}"})
-
-
-@app.route("/leaveProject/<int:projectId>", methods=["GET"])
-def leaveProject(projectId):
-    # Perform operations like checking if the user has joined the project
-    # and interacting with MongoDB if needed.
-    return jsonify({"message": f"Left Project {projectId}"})
-
-
-@app.route("/getProjects", methods=["PUT"])
+@app.route('/getProjects', methods=['PUT'])
 def get_projects():
     data = request.get_json()
-    user = data.get("user")
+    user = data.get('user')
 
     print(user)
     user_id = database.get_user_id(user)
@@ -107,6 +50,53 @@ def get_projects():
 
     return jsonify({"projects": projects, "success": True})
 
+@app.route('/checkout', methods=['PUT'])
+def checkout():
+    data = request.get_json()
+
+    project_id = data.get('projectId')
+    project = data.get('project')
+    hardware_index = data.get('hardwareIndex')
+    qty = data.get('qty')
+
+    if project == None or project_id == None or hardware_index == None or qty == None:
+        return jsonify({"success": False})
+
+    success, new_quantity = database.checkout_hardware(project_id, int(hardware_index), int(qty))
+
+    print(success)
+    print(project["hardwareSets"][hardware_index]["checkedOut"])
+    if success:
+        project["hardwareSets"][hardware_index]["checkedOut"] = new_quantity
+        available = project["hardwareSets"][hardware_index]["availability"]
+
+    for key in project.keys():
+        print(f"{key}: {project[key]}")
+
+
+
+    return jsonify({"success": success , "project": project, "availability": available})
+
+@app.route('/checkin1', methods=['PUT'])
+def checkin1():
+    data = request.get_json()
+
+    project = data.get('project')
+    project_id = data.get('projectId')
+    hardware = data.get('hardwareName')
+    qty = data.get('qty')
+    qty = int(qty)
+
+    print(f"qty: {qty}")
+    # print(f"hardware: {hardware}")
+    success = database.checkin_hardware(project_id, hardware, qty)
+
+    if success:
+        project["hardwareSets"][0]["checkedOut"] -= qty
+        available = project["hardwareSets"][0]["availability"]
+    
+    print(f"success: {success} , project: {project}, availability: {available}")
+    return jsonify({"success": success , "project": project, "availability": available})
 
 if __name__ == "__main__":
     database.initialize_database()
--- frontend/src/Components/Project/Project.jsx
+++ frontend/src/Components/Project/Project.jsx
@@ -1,191 +1,152 @@
 // Project.jsx
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Project.css';
 
-class Project extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValueCheckIn1: 0, // Update state property for check-in input
-      inputValueCheckOut1: 0, // Update state property for check-out input
-      inputValueCheckIn2: 0, // Update state property for check-in input
-      inputValueCheckOut2: 0, // Update state property for check-out input
-      showAuthorizedUsers: false, // Added state for dropdown
-      isJoined: true, // Added state for join/leave button
-    };
-  }
+const Project = ({ project: initialProject }) => {
+  const [project, setProject] = useState(initialProject);
 
-  handleInputChange = (e, hardwareSet, inputType) => {
-    this.setState({
-      [`${inputType}${hardwareSet}`]: parseInt(e.target.value),
+  const [inputValueCheckIn1, setInputValueCheckIn1] = useState(0);
+  const [inputValueCheckOut1, setInputValueCheckOut1] = useState(0);
+  const [inputValueCheckIn2, setInputValueCheckIn2] = useState(0);
+  const [inputValueCheckOut2, setInputValueCheckOut2] = useState(0);
+  const [showAuthorizedUsers, setShowAuthorizedUsers] = useState(false);
+  const [isJoined, setIsJoined] = useState(false);
+
+
+  const { projectId, projectName, hardwareSets, authorizedUsers } = project;
+
+  const { hardwareName: hardwareName1, totalCapacity: totalCapacity1, availability: initialAvailability1, checkedOut: checkedOut1 } = hardwareSets[0];
+  const { hardwareName: hardwareName2, totalCapacity: totalCapacity2, availability: initialAvailability2, checkedOut: checkedOut2 } = hardwareSets[1];
+
+  const [availability1, setAvailability1] = useState(initialAvailability1);
+  const [availability2, setAvailability2] = useState(initialAvailability2);
+
+  const handleCheckOut1 = async () => {
+    const response = await fetch('/checkout1', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        projectId: projectId,
+        qty: inputValueCheckOut1,
+        project: project
+      })
     });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setProject(data.project);
+      setAvailability1(data.availability);
+    }
+
   };
 
-  handleCheckIn = (hardwareSet) => {
-    const projectId = 1; // Replace with the actual project ID
-    const qty = this.state[`inputValueCheckIn${hardwareSet}`]; // Use the check-in input value
+  const handleCheckIn1 = async () => {
+    const response = await fetch('/checkin1', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        projectId: projectId,
+        qty: inputValueCheckIn1,
+        project: project
+      })
+    });
+    const data = await response.json();
 
-    // fetch(`/checkIn_hardware/${projectId}/${qty}`)
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     alert(data.message); // Display the response message in a popup
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error:', error);
-    //   });
+    if (data.success) {
+      setProject(data.project);
+      setAvailability1(data.availability);
+    }
   };
 
-  handleCheckOut = (hardwareSet) => {
-    const projectId = 1; // Replace with the actual project ID
-    const qty = this.state[`inputValueCheckOut${hardwareSet}`]; // Use the check-out input value
-
-    // fetch(`/checkOut_hardware/${projectId}/${qty}`)
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     alert(data.message); // Display the response message in a popup
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error:', error);
-    //   });
-  };
-
-  // Function to toggle join/leave button and change color
-  handleToggleJoinLeave = () => {
-    this.setState((prevState) => ({
-      isJoined: !prevState.isJoined,
-    }));
-    // fetch(`/join_project/${this.props.project.projectId}`) // Replace with the actual project ID
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     alert(data.message); // Display the response message in a popup
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error:', error);
-    //   });
-  };
-
-  // Function to toggle authorized users dropdown
-  handleToggleAuthorizedUsers = () => {
-    this.setState((prevState) => ({
-      showAuthorizedUsers: !prevState.showAuthorizedUsers,
-    }));
-  };
-
-  render() {
-    const {
-      project
-    } = this.props;
-
-    const {
-      projectId,
-      projectName,
-      authorizedUsers,
-      hardwareSets,
-    } = project; // Access props passed from Manager component
-
-    const {
-      hardwareName: hardwareName1,
-      totalCapacity: totalCapacity1,
-      availability: availability1,
-      checkedOut: checkedOut1,
-    } = hardwareSets[0];
-
-    const {
-      hardwareName: hardwareName2,
-      totalCapacity: totalCapacity2,
-      availability: availability2,
-      checkedOut: checkedOut2,
-    } = hardwareSets[1];
-
-    return (
-      <div className="project-container">
-        <h2>{`${projectName} (${projectId})`}</h2>
-        <div className="hardware-sets">
-          <div className="hardware-set-box">
-            <h3>{hardwareName1}</h3>
-            <div className="hardware-set">
-              <div className="label-value-box">
-                <div className="value-box">
-                  <div className="label">Availability</div>
-                  <div className="value">{`${availability1}/${totalCapacity1}`}</div>
-                </div>
-                <div className="value-box">
-                  <div className="label">Checked Out</div>
-                  <div className="value">{checkedOut1}</div>
-                </div>
+  return (
+    <div className="project-container">
+      <h2>{projectName}</h2>
+      <div className="hardware-sets">
+        <div className="hardware-set-box">
+          <h3>{hardwareName1}</h3>
+          <div className="hardware-set">
+            <div className="label-value-box">
+              <div className="value-box">
+                <div className="label">Availability</div>
+                <div className="value">{`${availability1}/${totalCapacity1}`}</div>
               </div>
-            </div>
-            <div className="hardware-controls">
-              <input
-                className='inputBox'
-                type="number"
-                name="inputValueCheckIn"
-                placeholder="0"  
-                onChange={(e) => this.handleInputChange(e, 'hardwareSet1', 'inputValueCheckIn')}
-              />
-              <button className="check-Button" onClick={() => this.handleCheckIn('hardwareSet1')}>Check In</button>
-              <input
-                type="number"
-                name="inputValueCheckOut"
-                placeholder="0"  
-                onChange={(e) => this.handleInputChange(e, 'hardwareSet1', 'inputValueCheckOut')}
-              />
-              <button className="check-Button" onClick={() => this.handleCheckOut('hardwareSet1')}>Check Out</button>
+              <div className="value-box">
+                <div className="label">Checked Out</div>
+                <div className="value">{checkedOut1}</div>
+              </div>
             </div>
           </div>
-          <div className="hardware-set-box">
-            <h3>{hardwareName2}</h3>
-            <div className="hardware-set">
-              <div className="label-value-box">
-                <div className="value-box">
-                  <div className="label">Availability</div>
-                  <div className="value">{`${availability2}/${totalCapacity2}`}</div>
-                </div>
-                <div className="value-box">
-                  <div className="label">Checked Out</div>
-                  <div className="value">{checkedOut2}</div>
-                </div>
-              </div>
-            </div>
-            <div className="hardware-controls">
-              <input
-                type="number"
-                name='inputValueCheckIn'
-                placeholder="0"  
-                onChange={(e) => this.handleInputChange(e, 'hardwareSet2', 'inputValueCheckIn')}
-              />
-              <button className="check-Button" onClick={() => this.handleCheckIn('hardwareSet2')}>Check In</button>
-              <input
-                type="number"
-                name="inputValueCheckOut"
-                placeholder="0"  
-                onChange={(e) => this.handleInputChange(e, 'hardwareSet2', 'inputValueCheckOut')}
-              />
-              <button className="check-Button" onClick={() => this.handleCheckOut('hardwareSet2')}>Check Out</button>
-            </div>
+          <div className="hardware-controls">
+            <input
+              className='inputBox'
+              type="number"
+              name="inputValueCheckIn"
+              onChange={(e) => setInputValueCheckIn1(e.target.value)}
+            />
+            <button className="check-Button" onClick={() => handleCheckIn1('hardwareSet1')}>Check In</button>
+            <input
+              type="number"
+              name="inputValueCheckOut"
+              onChange={(e) => setInputValueCheckOut1(e.target.value)}
+            />
+            <button className="check-Button" onClick={() => handleCheckOut1('hardwareSet1')}>Check Out</button>
           </div>
         </div>
-        <div className="button-row">
-          <button
-            className={this.state.isJoined ? 'leave-button' : 'join-button'} // Dynamically change button color
-            onClick={this.handleToggleJoinLeave}
-          >
-            {this.state.isJoined ? 'Leave Project' : 'Join Project'}
-          </button>
-          <button className="authorizedUsers-button" onClick={this.handleToggleAuthorizedUsers}>Authorized Users</button>
-          {this.state.showAuthorizedUsers && (
-            <div className="authorized-users-dropdown">
-              <ul>
-                {authorizedUsers.map((user, index) => (
-                  <li key={index}>{user}</li>
-                ))}
-              </ul>
+        <div className="hardware-set-box">
+          <h3>{hardwareName2}</h3>
+          <div className="hardware-set">
+            <div className="label-value-box">
+              <div className="value-box">
+                <div className="label">Availability</div>
+                <div className="value">{`${availability2}/${totalCapacity2}`}</div>
+              </div>
+              <div className="value-box">
+                <div className="label">Checked Out</div>
+                <div className="value">{checkedOut2}</div>
+              </div>
             </div>
-          )}
+          </div>
+          <div className="hardware-controls">
+            <input
+              type="number"
+              name='inputValueCheckIn'
+              onChange={(e) => setInputValueCheckIn2(e.target.value)}
+            />
+            <button className="check-Button" onClick={() => handleCheckIn1('hardwareSet2')}>Check In</button>
+            <input
+              type="number"
+              name="inputValueCheckOut"
+              onChange={(e) => setInputValueCheckOut2(e.target.value)}
+            />
+            <button className="check-Button" onClick={() => handleCheckOut1('hardwareSet2')}>Check Out</button>
+          </div>
         </div>
       </div>
-    );
-  }
+      <div className="button-row">
+        <button
+          className={isJoined ? 'leave-button' : 'join-button'} // Dynamically change button color
+          onClick={handleToggleJoinLeave}
+        >
+          {isJoined ? 'Leave Project' : 'Join Project'}
+        </button>
+        <button className="authorizedUsers-button" onClick={handleToggleAuthorizedUsers}>Authorized Users</button>
+        {showAuthorizedUsers && (
+          <div className="authorized-users-dropdown">
+            <ul>
+              {authorizedUsers.map((user, index) => (
+                <li key={index}>{user}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Project;
+export default Project; 
